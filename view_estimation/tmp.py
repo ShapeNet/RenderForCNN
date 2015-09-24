a54d75e855a258acf9c86a81822c016b7e654d11
Charles Qi <rqi@oriong.stanford.edu>
2015-09-23 23:43:24 -0700

    added viewpoint demo

--- view_estimation/tmp.py
+++
@@ -1,8 +0,0 @@
-from evaluation_helper import *
-
-img_name_file = '../data/real_images/voc12val_easy_gt_bbox/chair.txt'
-
-img_filenames = [x.rstrip().split(' ')[0] for x in open(img_name_file)]
-class_idxs = [int(class_idx) for _ in range(len(img_filenames))]
-
-viewpoint(img_filenames, class_idxs,'chair_preds.txt')
